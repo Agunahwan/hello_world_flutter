37ca36224cd2ecac75a8c8e0581a9a4fcba46e72
Agunahwan Absin <vair.agun@gmail.com>
2022-02-08 23:44:06 +0700

    Initial Version

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Apps"),
+        ),
+        body: Center(
+          child: Text(
+            "HALO",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                backgroundColor: Colors.amber,
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3),
+          ),
+        ),
+      ),
+    );
+  }
+}
