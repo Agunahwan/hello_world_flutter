ded0eb47b92b99594affbe92af8cd8184ccb18bd
Agunahwan Absin <vair.agun@gmail.com>
2022-02-09 00:09:38 +0700

    Invisible Widget & Container

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,52 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My Apps"),
+          title: Text("Column"),
         ),
-        body: Center(
-          child: Text(
-            "HALO",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              backgroundColor: Colors.amber,
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-              fontFamily: 'Stick',
-              decoration: TextDecoration.lineThrough,
-              decorationStyle: TextDecorationStyle.wavy,
-              decorationColor: Colors.white,
-              decorationThickness: 2,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              width: 200,
+              height: 50,
+              color: Colors.green,
             ),
-          ),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 50,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 300,
+              height: 50,
+              color: Colors.red,
+            ),
+          ],
         ),
+        // body: Center(
+        //   child: Text(
+        //     "HALO",
+        //     textAlign: TextAlign.center,
+        //     style: TextStyle(
+        //       backgroundColor: Colors.amber,
+        //       color: Colors.white,
+        //       fontSize: 30,
+        //       fontWeight: FontWeight.bold,
+        //       letterSpacing: 3,
+        //       fontFamily: 'Stick',
+        //       decoration: TextDecoration.lineThrough,
+        //       decorationStyle: TextDecorationStyle.wavy,
+        //       decorationColor: Colors.white,
+        //       decorationThickness: 2,
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
